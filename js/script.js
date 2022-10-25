51cf10b4f57da3afc952e4ec87311d919eb80f04
Erika Toranzo <erikatoranzo@hotmail.com>
2022-10-25 10:58:21 -0300

    TP js v3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 // EVENTO ASOCIADO AL ONCLICK DEL BOTON RESUMEN
 function getResumen() {
-    document.getElementById("monto_total").innerHTML = "Total a pagar: $";
+    if (document.getElementById("cantidad").value == "") {
+        //ACTUALIZACION TEXTO
+        document.getElementById("monto_total").innerHTML = "Total a Pagar: $";
+    }
+    else {
+        //TOMAMOS EL VALOR TOTAL DEL TICKET POR LA CANTIDAD DE TICKETS COMPRADOS
+        var valor_total_ticket = parseInt(document.getElementById("cantidad").value) * 200;
+        //PORCENTAJE ASOCIADO A CADA CATEGORIA
+        var porcentaje_categoria_seleccionada = parseInt(document.getElementById("categoria_id").value);
+        //CALCULO MONTO A DESCONTAR
+        var monto_descuento = ((porcentaje_categoria_seleccionada * valor_total_ticket) / 100).toFixed(2);
+        //CALCULO MONTO A PAGAR
+        var monto_pagar = valor_total_ticket - monto_descuento;
+        //ACTUALIZACION TEXTO
+        document.getElementById("monto_total").innerHTML = "Total a Pagar: $" + monto_pagar;
+    }
 }
